ddd3c9b7edb2f26065592391e8010e3bc208f67a
SamuelWillis <samuel.w.h.willis@gmail.com>
2023-07-05 18:13:52 -0700

    💅 Add basic home page screen
    Add a basic home page screen so that my website is no longer an add for
    the Phoenix framework!

--- test/samuel_willis_web/controllers/page_controller_test.exs
+++ test/samuel_willis_web/controllers/page_controller_test.exs
@@ -1,8 +1,15 @@
 defmodule SamuelWillisWeb.PageControllerTest do
   use SamuelWillisWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  describe "home/2" do
+    test "renders successfully", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      assert html = html_response(conn, 200)
+
+      assert html =~ "Samuel Willis"
+      assert html =~ "https://github.com/SamuelWillis"
+      assert html =~ "https://www.linkedin.com/in/willissamuel/"
+      assert html =~ "mailto:samuel.w.h.willis@gmail.com"
+    end
   end
 end
