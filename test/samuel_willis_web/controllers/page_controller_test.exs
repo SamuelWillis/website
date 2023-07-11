ea664f5dfe5a093ceb54fbcb1d1d9c629210f6bc
Samuel Willis <samuel.w.h.willis@gmail.com>
2023-07-10 23:05:31 -0700

    🔗 Add link to articles to home page

--- test/samuel_willis_web/controllers/page_controller_test.exs
+++ test/samuel_willis_web/controllers/page_controller_test.exs
@@ -7,6 +7,12 @@ defmodule SamuelWillisWeb.PageControllerTest do
       assert html = html_response(conn, 200)
 
       assert html =~ "Samuel Willis"
+
+      # Assert links to articles appears
+      assert html =~ "Articles"
+      assert html =~ "/articles"
+
+      # Assert social links are on screen
       assert html =~ "https://github.com/SamuelWillis"
       assert html =~ "https://www.linkedin.com/in/willissamuel/"
       assert html =~ "mailto:samuel.w.h.willis@gmail.com"
