ad2d0481d0eeff6d02701d1b11dd4a38fe0d275b
SamuelWillis <samuel.w.h.willis@gmail.com>
2023-07-20 09:38:42 -0700

    Adjust `@tailwind/typography` pre styles
    Adjust `@tailwind/typography` pre styles to match the background output
    by makeup.

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -18,21 +18,24 @@ module.exports = {
       },
       typography:(theme) => ({
         DEFAULT: {
-        css: {
-          a: {
-            "border-radius": theme("borderRadius.sm"),
-            "&:hover": {
-              color: theme("colors.indigo.500"),
+          css: {
+            a: {
+              "border-radius": theme("borderRadius.sm"),
+              "&:hover": {
+                color: theme("colors.indigo.500"),
+              },
+              "&:focus": {
+                color: theme("colors.indigo.500"),
+                "outline-color": theme("colors.indigo.500"),
+                "outline-width": theme("outlineWidth.2"),
+                "outline-offset": theme("outlineOffset.0"),
+                "outline-style": "dashed",
+              },
             },
-            "&:focus": {
-              color: theme("colors.indigo.500"),
-              "outline-color": theme("colors.indigo.500"),
-              "outline-width": theme("outlineWidth.2"),
-              "outline-offset": theme("outlineOffset.0"),
-              "outline-style": "dashed",
-            },
+            pre: {
+              "background-color": "#F0F0F0"
+            }
           },
-        },
         },
       }),
     },
