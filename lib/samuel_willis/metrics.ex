7ea4c892674f47ff1cccefdbde78085d5ae84197
samuel <samuel.w.h.willis@gmail.com>
2023-08-27 15:07:19 -0700

    Add the `Metrics.upsert/2` function
    Add the `Metrics.upsert/2` function to handle upserts of Metrics.
    
    With this function it will be easy to insert new metrics or update
    existing ones as needed.
    
    Inspired by [this Dashbit
    article](https://dashbit.co/blog/homemade-analytics-with-ecto-and-elixir)
    I decided to roll my own analytics based on their approach.

--- lib/samuel_willis/metrics.ex
+++ lib/samuel_willis/metrics.ex
@@ -2,4 +2,31 @@ defmodule SamuelWillis.Metrics do
   @moduledoc """
   A minimal webpage metrics engine
   """
+
+  alias SamuelWillis.Repo
+  alias SamuelWillis.Metrics.Metric
+
+  @doc """
+  Upsert a SamuelWillis.Metrics.Metric for the given path
+
+  Upserts the given path's metrics with the provided `visits` count.
+
+  *Note:* In the case of an upsert, the updated metric will not be returned.
+  [See the docs](https://hexdocs.pm/ecto/Ecto.Repo.html#c:insert/2-upserts)
+
+  Since these upserts are expected to occur in the background, it is not
+  expected that the return of the upsert will be used. Thus it is acceptable
+  (for now) that the returned schema is out of sync with the DB. If this
+  assumption changes one of the suggested strategies in the ddocs will need to
+  be employed.
+  """
+  def upsert!(path, visits \\ 1) do
+    date = Date.utc_today()
+
+    Repo.insert!(
+      %Metric{date: date, path: path, visits: visits},
+      on_conflict: [inc: [visits: visits]],
+      conflict_target: [:date, :path]
+    )
+  end
 end
