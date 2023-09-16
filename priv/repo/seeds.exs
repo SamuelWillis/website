502943f0d82876fec89d4fa2b7d79a0f513b053b
samuel <samuel.w.h.willis@gmail.com>
2023-09-15 20:43:35 -0700

    Add Metrics seed data
    Add some Metrics seed data so that it is easy to generate some fake
    metrics in the development database.
    
    This will make building out the metrics panel quite a bit easier as it
    will provide an automated way to get some sample data in to the DB.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+paths = ["/", "/articles", "/about"]
+today = Date.utc_today()
+
+metric_attrs =
+  for number_of_days_ago <- -1..-7, path <- paths, into: [] do
+    date = Date.add(today, number_of_days_ago)
+    visits = Enum.random(1..20)
+
+    %{
+      date: date,
+      path: path,
+      visits: visits
+    }
+  end
+
+SamuelWillis.Repo.insert_all(SamuelWillis.Metrics.Metric, metric_attrs)
