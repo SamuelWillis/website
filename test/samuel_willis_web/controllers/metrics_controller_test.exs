1f40f559c28369fa85f65b11e51167face7b71cd
samuel <samuel.w.h.willis@gmail.com>
2023-09-21 19:45:19 -0700

    Update Metrics index with weekly metrics
    Update the Metrics index to display the weekly metrics.
    
    This is a first pass on showing "useful" metrics, some thought needs to
    be put into what would be nice to see on this screen.
    
    Likely, it should be something like most popular pages for week, month,
    all time or something like that.
    
    For now it's just what's gone on this week.

--- test/samuel_willis_web/controllers/metrics_controller_test.exs
+++ test/samuel_willis_web/controllers/metrics_controller_test.exs
@@ -7,39 +7,44 @@ defmodule SamuelWillisWeb.MetricsControllerTest do
 
       html = html_response(conn, 200)
 
+      assert html =~ "Metrics!"
       assert html =~ "No metrics to display"
     end
 
-    test "renders metrics", %{conn: conn} do
-      today_metric = build_metric(path: "/today")
-
-      yesterday_metric = build_metric(date: Date.add(Date.utc_today(), -1), path: "/yesterday")
+    test "renders metrics from last week", %{conn: conn} do
+      build_metrics()
 
       conn = get(conn, ~p"/metrics")
 
       html = html_response(conn, 200)
 
       today_string = Date.utc_today() |> Date.to_string()
-      yesterday_string = Date.utc_today() |> Date.add(-1) |> Date.to_string()
+      seven_days_ago_string = Date.utc_today() |> Date.add(-7) |> Date.to_string()
 
-      assert html =~ "#{today_string} #{today_metric.path}: #{today_metric.visits}"
-      assert html =~ "#{yesterday_string} #{yesterday_metric.path}: #{yesterday_metric.visits}"
+      assert html =~ "Metrics!"
+      assert html =~ "#{today_string}"
+      assert html =~ "#{seven_days_ago_string}"
 
       refute html =~ "No metrics to display"
     end
   end
 
-  @default_attrs %{
-    date: Date.utc_today(),
-    path: "/some_path",
-    visits: 10
-  }
+  @paths ["/", "/articles", "/about"]
+  def build_metrics do
+    today = Date.utc_today()
 
-  def build_metric(attrs \\ %{}) do
-    attrs = Enum.into(attrs, @default_attrs)
+    metric_attrs =
+      for number_of_days_ago <- 0..-7, path <- @paths, into: [] do
+        date = Date.add(today, number_of_days_ago)
+        visits = Enum.random(1..20)
 
-    %SamuelWillis.Metrics.Metric{}
-    |> Ecto.Changeset.change(attrs)
-    |> SamuelWillis.Repo.insert!()
+        %{
+          date: date,
+          path: path,
+          visits: visits
+        }
+      end
+
+    SamuelWillis.Repo.insert_all(SamuelWillis.Metrics.Metric, metric_attrs)
   end
 end
