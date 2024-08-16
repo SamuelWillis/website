57c553d2f2519fd0ebcf41f74ec60e5f67722d98
SamuelWillis <hello@samuelwillis.dev>
2024-08-16 08:28:40 -0700

    Convert main website pages to LiveView
    Convert the main website pages to LiveView as I would like to begin
    adding some slick new behaviour to these screens.
    
    This migration required some adjustments to the app layout to made in
    order to support the dynamic page views counter.
    
    With the swap to LiveViews, the main pages are ran inside of a
    `live_session` block.
    This means that when navigating between the `/`, `/now`, and `/about`
    pages the Plug pipeline is called exactly once.
    Normally this would not be an issue but since the `page_visits`
    component gets its assigns from the Plug pipeline, it meant that the
    page visits count would never be updated when navigating between the
    pages and the wrong count would be displayed.
    
    In order to resolve this two things needed to be done.
    
    First a LiveView hook was added that sets the `page_visits` assign in
    the `handle_params/3` callback.
    The `handle_params/3` callback is used as it is the only time in the
    lifecycle that the URI is available and the Metrics are counted based on
    the URI path.
    
    Second, the `app.html.heex` template was updated to house the `header`,
    `main`, and `footer` elements from the `root.html.heex`.
    This was done as the `root.html.heex` is outside of the DOM that
    LiveView has access to.
    Thus, if we want to be able to dynamically change the
    `Layout.page_visits/1` display state, it needs to be housed inside of
    the DOM that LiveView has access to.
    I don't _love_ this but it works so 🤷‍♂️

---
+++ lib/samuel_willis_web/live/HomeLive.ex
@@ -0,0 +1,64 @@
+defmodule SamuelWillisWeb.HomeLive do
+  use SamuelWillisWeb, :live_view
+
+  @images [
+    %{name: "mammoth.jpg", alt: "Image of a snowy mountain in the Canadian Rockies"},
+    %{name: "mountain.jpg", alt: "Image of a snow capped mountain in British Columbia"},
+    %{name: "road.jpg", alt: "Image of an abandoned road"},
+    %{name: "rock.jpg", alt: "Image of a person bouldering"},
+    %{name: "train.jpg", alt: "Image of a train track crossing above a valley river"},
+    %{name: "valley.jpg", alt: "Image of a valley with a large river in it"},
+    %{name: "waterfall.jpg", alt: "Image of a waterfall"}
+  ]
+
+  def render(assigns) do
+    ~H"""
+    <div class="flex flex-col items-center sm:grid sm:grid-cols-2 sm:gap-6 space-y-6">
+      <.home_image />
+
+      <nav class="flex flex-col items-center">
+        <ul class="space-y-4 text-xl font-bold">
+          <li>
+            <.link
+              navigate={~p"/now"}
+              class="px-4 py-2 font-semibold text-xl rounded-sm
+              hover:text-indigo-800
+              focus:text-indigo-800 focus:outline-dashed focus:outline-2 focus:outline-offset-0 focus:outline-indigo-500"
+            >
+              Now
+            </.link>
+          </li>
+          <li>
+            <.link
+              navigate={~p"/about"}
+              class="px-4 py-2 font-semibold text-xl rounded-sm
+              hover:text-indigo-800
+              focus:text-indigo-800 focus:outline-dashed focus:outline-2 focus:outline-offset-0 focus:outline-indigo-500"
+            >
+              About
+            </.link>
+          </li>
+          <li>
+            <.link
+              navigate={~p"/articles"}
+              class="px-4 py-2 font-semibold text-xl rounded-sm
+              hover:text-indigo-800
+              focus:text-indigo-800 focus:outline-dashed focus:outline-2 focus:outline-offset-0 focus:outline-indigo-500"
+            >
+              Articles
+            </.link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    """
+  end
+
+  def home_image(assigns) do
+    assigns = assign(assigns, :image, Enum.random(@images))
+
+    ~H"""
+    <img src={~p"/images/#{@image.name}"} class="w-96" alt={@image.alt} />
+    """
+  end
+end
