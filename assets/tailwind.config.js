bcccc7d764bfc934dd1a2230cb898ffc38eb046c
SamuelWillis <hello@samuelwillis.dev>
2025-07-10 21:01:12 -0700

    Update to LiveView 1.0

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,98 +1,95 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-const plugin = require("tailwindcss/plugin")
-const fs = require("fs")
-const path = require("path")
+const plugin = require("tailwindcss/plugin");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   content: [
     "./js/**/*.js",
-    "../lib/*_web.ex",
-    "../lib/*_web/**/*.*ex"
+    "../lib/samuel_willis_web.ex",
+    "../lib/samuel_web/**/*.*ex",
   ],
   theme: {
     extend: {
       colors: {
         brand: "#FD4F00",
       },
-      typography:(theme) => ({
-        DEFAULT: {
-          css: {
-            a: {
-              "border-radius": theme("borderRadius.sm"),
-              "&:hover": {
-                color: theme("colors.indigo.500"),
-              },
-              "&:focus": {
-                color: theme("colors.indigo.500"),
-                "outline-color": theme("colors.indigo.500"),
-                "outline-width": theme("outlineWidth.2"),
-                "outline-offset": theme("outlineOffset.0"),
-                "outline-style": "dashed",
-              },
-            },
-            pre: {
-              "background-color": "#F0F0F0",
-              color: "currentcolor"
-            },
-            code: null,
-            'code::before': null,
-            'code::after': null,
-            'pre code': null,
-            'pre code::before': null,
-            'pre code::after': null,
-          },
-        },
-      }),
     },
   },
   plugins: [
     require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
     // Allows prefixing tailwind classes with LiveView classes to add rules
     // only when LiveView classes are applied, for example:
     //
     //     <div class="phx-click-loading:animate-ping">
     //
-    plugin(({addVariant}) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
-    plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"])),
+    plugin(({ addVariant }) =>
+      addVariant("phx-click-loading", [
+        ".phx-click-loading&",
+        ".phx-click-loading &",
+      ]),
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-submit-loading", [
+        ".phx-submit-loading&",
+        ".phx-submit-loading &",
+      ]),
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-change-loading", [
+        ".phx-change-loading&",
+        ".phx-change-loading &",
+      ]),
+    ),
 
     // Embeds Heroicons (https://heroicons.com) into your app.css bundle
     // See your `CoreComponents.icon/1` for more information.
     //
-    plugin(function({matchComponents, theme}) {
-      let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized")
-      let values = {}
+    plugin(function ({ matchComponents, theme }) {
+      let iconsDir = path.join(__dirname, "../deps/heroicons/optimized");
+      let values = {};
       let icons = [
         ["", "/24/outline"],
         ["-solid", "/24/solid"],
-        ["-mini", "/20/solid"]
-      ]
+        ["-mini", "/20/solid"],
+        ["-micro", "/16/solid"],
+      ];
       icons.forEach(([suffix, dir]) => {
-        fs.readdirSync(path.join(iconsDir, dir)).map(file => {
-          let name = path.basename(file, ".svg") + suffix
-          values[name] = {name, fullPath: path.join(iconsDir, dir, file)}
-        })
-      })
-      matchComponents({
-        "hero": ({name, fullPath}) => {
-          let content = fs.readFileSync(fullPath).toString().replace(/\r?\n|\r/g, "")
-          return {
-            [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
-            "-webkit-mask": `var(--hero-${name})`,
-            "mask": `var(--hero-${name})`,
-            "mask-repeat": "no-repeat",
-            "background-color": "currentColor",
-            "vertical-align": "middle",
-            "display": "inline-block",
-            "width": theme("spacing.5"),
-            "height": theme("spacing.5")
-          }
-        }
-      }, {values})
-    })
-  ]
-}
+        fs.readdirSync(path.join(iconsDir, dir)).forEach((file) => {
+          let name = path.basename(file, ".svg") + suffix;
+          values[name] = { name, fullPath: path.join(iconsDir, dir, file) };
+        });
+      });
+      matchComponents(
+        {
+          hero: ({ name, fullPath }) => {
+            let content = fs
+              .readFileSync(fullPath)
+              .toString()
+              .replace(/\r?\n|\r/g, "");
+            let size = theme("spacing.6");
+            if (name.endsWith("-mini")) {
+              size = theme("spacing.5");
+            } else if (name.endsWith("-micro")) {
+              size = theme("spacing.4");
+            }
+            return {
+              [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
+              "-webkit-mask": `var(--hero-${name})`,
+              mask: `var(--hero-${name})`,
+              "mask-repeat": "no-repeat",
+              "background-color": "currentColor",
+              "vertical-align": "middle",
+              display: "inline-block",
+              width: size,
+              height: size,
+            };
+          },
+        },
+        { values },
+      );
+    }),
+  ],
+};
