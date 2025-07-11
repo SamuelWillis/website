3ab6ec73b92a236149227b171b5706e69619e28c
SamuelWillis <hello@samuelwillis.dev>
2025-07-10 22:25:42 -0700

    First attempt
    Used this prompt:
    ```
    I would like to turn the randomly selected image on the
    [@HomeLive.ex](@file:website/lib/samuel_willis_web/live/HomeLive.ex)
    into a clickable image that opens up a carosel style gallery that allows
    viewing all the images.
    
    The current image should display above a row of small thumbnails below
    the image. There should be a left and right icon to either side of the
    main image. The left and right arrows should circle "infinitely", ie:
    pressing right on the last image takes you to the first image. Pressing
    left on the first image takes you to the last image.  The images should
    display overtop of a dark gray overlay.
    
    Write this using function components in the `HomeLive`. Ask clarifying
    questions before you start
    ```
    
    It had some errors that I had to prompt it through. Keyboard navigation
    does not work.
    Mobile screens aren't bad either.

--- lib/samuel_willis_web/live/HomeLive.ex
+++ lib/samuel_willis_web/live/HomeLive.ex
@@ -14,7 +14,13 @@ defmodule SamuelWillisWeb.HomeLive do
   ]
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :image, Enum.random(@images))}
+    {:ok,
+     assign(socket,
+       image: Enum.random(@images),
+       gallery_open: false,
+       current_image_index: 0,
+       images: @images
+     )}
   end
 
   def render(assigns) do
@@ -22,9 +28,10 @@ defmodule SamuelWillisWeb.HomeLive do
     <div class="flex-1 flex flex-col items-center sm:grid sm:grid-cols-2 sm:gap-6 space-y-6">
       <img
         src={~p"/images/#{@image.name}"}
-        class="hidden w-96"
+        class="hidden w-96 cursor-pointer"
         alt={@image.alt}
         phx-connected={JS.show()}
+        phx-click="open_gallery"
       />
       <div class="w-96 h-full flex justify-center items-center" phx-connected={JS.hide()}>
         <svg
@@ -79,13 +86,140 @@ defmodule SamuelWillisWeb.HomeLive do
         </ul>
       </nav>
     </div>
+
+    <.gallery :if={@gallery_open} images={@images} current_index={@current_image_index} />
     """
   end
 
-  attr :image, :map, required: true
+  def handle_event("open_gallery", _params, socket) do
+    # Find the index of the currently displayed image
+    current_index =
+      Enum.find_index(@images, fn img -> img.name == socket.assigns.image.name end) || 0
 
-  def home_image(assigns) do
+    {:noreply, assign(socket, gallery_open: true, current_image_index: current_index)}
+  end
+
+  def handle_event("close_gallery", _params, socket) do
+    {:noreply, assign(socket, gallery_open: false)}
+  end
+
+  def handle_event("prev_image", _params, socket) do
+    new_index =
+      if socket.assigns.current_image_index == 0 do
+        length(@images) - 1
+      else
+        socket.assigns.current_image_index - 1
+      end
+
+    {:noreply, assign(socket, current_image_index: new_index)}
+  end
+
+  def handle_event("next_image", _params, socket) do
+    new_index =
+      if socket.assigns.current_image_index == length(@images) - 1 do
+        0
+      else
+        socket.assigns.current_image_index + 1
+      end
+
+    {:noreply, assign(socket, current_image_index: new_index)}
+  end
+
+  def handle_event("select_image", %{"index" => index}, socket) do
+    {:noreply, assign(socket, current_image_index: String.to_integer(index))}
+  end
+
+  def handle_event("keydown", %{"key" => "Escape"}, socket) do
+    {:noreply, assign(socket, gallery_open: false)}
+  end
+
+  def handle_event("keydown", %{"key" => "ArrowLeft"}, socket) do
+    if socket.assigns.gallery_open do
+      handle_event("prev_image", %{}, socket)
+    else
+      {:noreply, socket}
+    end
+  end
+
+  def handle_event("keydown", %{"key" => "ArrowRight"}, socket) do
+    if socket.assigns.gallery_open do
+      handle_event("next_image", %{}, socket)
+    else
+      {:noreply, socket}
+    end
+  end
+
+  def handle_event("keydown", _params, socket) do
+    {:noreply, socket}
+  end
+
+  attr :images, :list, required: true
+  attr :current_index, :integer, required: true
+
+  def gallery(assigns) do
     ~H"""
+    <div
+      class="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50"
+      phx-click="close_gallery"
+      phx-window-keydown="keydown"
+      phx-key="Escape"
+    >
+      <div class="max-w-7xl max-h-full w-full h-full flex flex-col items-center justify-center p-4">
+        <!-- Close button -->
+        <button
+          class="absolute top-4 right-4 text-white text-2xl hover:text-gray-300 z-60"
+          phx-click="close_gallery"
+          aria-label="Close gallery"
+        >
+          ×
+        </button>
+        
+    <!-- Main image container -->
+        <div class="relative flex-1 flex items-center justify-center w-full max-h-[calc(100vh-120px)]">
+          <!-- Previous button -->
+          <button
+            class="absolute left-4 top-1/2 transform -translate-y-1/2 text-white text-4xl hover:text-gray-300 z-10"
+            phx-click="prev_image"
+            aria-label="Previous image"
+          >
+            ‹
+          </button>
+          
+    <!-- Current image -->
+          <img
+            src={~p"/images/#{Enum.at(@images, @current_index).name}"}
+            alt={Enum.at(@images, @current_index).alt}
+            class="max-w-full max-h-full object-contain"
+            phx-click={JS.push("next_image")}
+          />
+          
+    <!-- Next button -->
+          <button
+            class="absolute right-4 top-1/2 transform -translate-y-1/2 text-white text-4xl hover:text-gray-300 z-10"
+            phx-click="next_image"
+            aria-label="Next image"
+          >
+            ›
+          </button>
+        </div>
+        
+    <!-- Thumbnails -->
+        <div class="flex space-x-2 mt-4 overflow-x-auto max-w-full">
+          <div :for={{image, index} <- Enum.with_index(@images)} class="flex-shrink-0">
+            <img
+              src={~p"/images/#{image.name}"}
+              alt={image.alt}
+              class={[
+                "w-16 h-16 object-cover cursor-pointer border-2 hover:border-white transition-colors",
+                if(index == @current_index, do: "border-white", else: "border-gray-600")
+              ]}
+              phx-click="select_image"
+              phx-value-index={index}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
     """
   end
 end
