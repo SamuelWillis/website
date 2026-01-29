89aba7f5ecad2140494fa2750e7beda9df8def4e
SamuelWillis <hello@samuelwillis.dev>
2026-01-28 20:39:57 -0800

    Add MDEx markdown converter
    Add the MDEx mardown converter because it looks neat.
    
    Also drops a bunch of makeup config that's no longer needed. Not sure
    why it isn't? Maybe I'm just okay with the styling now...

---
+++ lib/samuel_willis/blog/mdex_converter.ex
@@ -0,0 +1,16 @@
+defmodule SamuelWillis.Blog.MDExConverter do
+  @moduledoc """
+  Markdown to HTML conversion powered by MDEx.
+
+  [Docs](https://github.com/dashbitco/nimble_publisher?tab=readme-ov-file#custom-html-converter)
+  """
+  alias NimblePublisher
+
+  def convert(filepath, body, _attrs, opts) do
+    if Path.extname(filepath) in [".md", ".markdown"] do
+      highlighters = Keyword.get(opts, :highlighters, [])
+
+      body |> MDEx.to_html!() |> NimblePublisher.highlight(highlighters)
+    end
+  end
+end
