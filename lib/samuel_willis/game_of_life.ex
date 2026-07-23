a67691c1a44cb72ced50ee09384b8b0e0d5a4e65
Samuel Willis <hello@samuelwillis.dev>
2026-07-22 23:23:04 -0700

    Render the Game Of Life
    Holy this is a naive first pass but it does the thing using a canvas.
    
    There's almost DEFINITELY a way I can be updating the canvas more better
    to ensure we aren't rebuilding the thing each time and so that it scales
    well and so that it's not a fixed 300 x 300 canvas and so that it
    handles window resizing.
    
    These are for future Sam because now this first pass is working.
    
    Things I would like to do:
    
    1. Full screen canvas
    2. Scale the cells based on initial universe and cavas state
    3. Handle screen resizes
    4. Center the board on the canvas
    5. Handle things going off screen
        * scale cells down to a min
        * skip rendering when they are too far away
    6. Allow repositioning the canvas?
    
    Anyways, this is a super rough start and it's actually pretty fun to
    work on.

--- lib/samuel_willis/game_of_life.ex
+++ lib/samuel_willis/game_of_life.ex
@@ -13,30 +13,35 @@ defmodule SamuelWillis.GameOfLife do
     |> Universe.build()
   end
 
-  def start(seed_name) when seed_name in @valid_seeds do
+  def tick(%Universe{} = universe) do
+    Universe.next_generation(universe)
+  end
+
+  def simulate(seed_name) when seed_name in @valid_seeds do
     universe = build(seed_name)
 
-    tick(universe)
+    do_simulate(universe)
+  end
+
+  # Hardcode 10 generation loop
+  defp do_simulate(%{generation: 20} = universe), do: universe
+
+  defp do_simulate(universe) do
+    universe |> Universe.next_generation() |> do_simulate()
   end
 
   defp get_seed(:t_tetromino) do
     [
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 1, 1, 1, 0, 0, 0],
-      [0, 0, 0, 0, 1, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
-  end
-
-  # Hardcode 10 generation loop
-  defp tick(%{generation: 20} = universe), do: universe
-
-  defp tick(universe) do
-    universe |> Universe.next_generation() |> tick()
   end
 end
--- lib/samuel_willis_web/live/game_of_life_live.ex
+++ lib/samuel_willis_web/live/game_of_life_live.ex
@@ -8,19 +8,114 @@ defmodule SamuelWillisWeb.GameOfLifeLive do
     ~H"""
     <Layouts.life flash={@flash}>
       <div class="flex-1 flex justify-center items-center">
-        🏗️
+        <canvas
+          id="universe"
+          width="300"
+          height="300"
+          phx-hook=".Universe"
+          phx-update="ignore"
+          data-cells={Jason.encode!(@cells)}
+        ></canvas>
+        <div class="absolute bottom-4 right-4 flex gap-4">
+          <button class="btn btn-ghost btn-error" phx-click="reset">Reset</button>
+          <button class="btn btn-primary" phx-click="start" disabled={@simulating}>Start</button>
+        </div>
       </div>
+
+      <script :type={Phoenix.LiveView.ColocatedHook} name=".Universe">
+        export default {
+          ctx() {
+            return this.el.getContext('2d');
+          },
+          cells() {
+            return JSON.parse(this.el.dataset.cells);
+          },
+          mounted() {
+            this.renderCells();
+          },
+          updated() {
+            // Request an animation frame from the browser so that we are not
+            // forcing the browser to rerender when it does not need to.
+            if (this.animationFrameRequest) {
+              cancelAnimationFrame(this.animationFrameRequest);
+            }
+            this.animationFrameRequest = requestAnimationFrame(() => this.renderCells());
+          },
+          renderCells() {
+            const ctx = this.ctx()
+            const cells = this.cells();
+
+            ctx.clearRect(0, 0, this.el.width, this.el.height);
+
+            for (let i = 0; i < cells.length; i++) {
+              for (let j = 0; j < cells[i].length; j++) {
+                let cell = cells[i][j];
+                if (cells[i][j] === 0) {
+                  continue;
+                }
+                ctx.fillStyle = cells[i][j] === 0 ? 'white' : 'green';
+                ctx.fillRect(j * 30, i * 30, 30, 30)
+              }
+            }
+          }
+        }
+      </script>
     </Layouts.life>
     """
   end
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
+    universe = GameOfLife.build(:t_tetromino)
+    cells = universe.cells |> Tuple.to_list() |> Enum.map(&Tuple.to_list/1)
+
     socket =
       socket
       |> assign(:page_title, "Game of Life")
-      |> assign(:universe, GameOfLife.build(:t_tetromino))
+      |> assign(:universe, universe)
+      |> assign(:cells, cells)
+      |> assign(:simulating, false)
 
     {:ok, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("start", _unsigned_params, socket) do
+    Process.send_after(self(), :tick, 100)
+    {:noreply, assign(socket, :simulating, true)}
+  end
+
+  def handle_event("reset", _unsigned_params, socket) do
+    universe = GameOfLife.build(:t_tetromino)
+    cells = universe.cells |> Tuple.to_list() |> Enum.map(&Tuple.to_list/1)
+
+    socket =
+      socket
+      |> assign(:universe, universe)
+      |> assign(:cells, cells)
+      |> assign(:simulating, false)
+
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_info(:tick, socket) do
+    universe = GameOfLife.tick(socket.assigns.universe)
+    cells = universe.cells |> Tuple.to_list() |> Enum.map(&Tuple.to_list/1)
+
+    case universe do
+      %{generation: 20} ->
+        {:noreply, socket}
+
+      _ ->
+        socket =
+          socket
+          |> assign(:universe, universe)
+          |> assign(:cells, cells)
+
+        Process.send_after(self(), :tick, 500)
+
+        {:noreply, socket}
+    end
   end
 end
